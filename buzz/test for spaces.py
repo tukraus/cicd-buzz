21544eba2e8b43e828eb470fb4186f9f040c4fa6
Thomas Kraus <19190080+tukraus@users.noreply.github.com>
2019-12-10 22:39:07 +0100

    introduce duplication

--- buzz/test for spaces.py
+++ buzz/test for spaces.py
@@ -17,6 +17,12 @@ def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
     return phrase.title()
 
 if __name__ == "__main__":
