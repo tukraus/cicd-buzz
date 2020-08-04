a7db74566e7c010e3f994c8a2afbb79e707f3a41
Thomas Kraus <19190080+tukraus@users.noreply.github.com>
2020-08-04 17:56:54 +0200

    Update duplicated.py

--- buzz/duplicated.py
+++ buzz/duplicated.py
@@ -209,6 +209,12 @@ def generate_buzz():
         sample(verbs), buzz_terms[1]])
     return phrase.title()
 
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+        sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
 buzz = ('continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
